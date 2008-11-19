1ed50e3b4162ac0617d4244ee610514a3613e759
Kalpana Thakur <kalpana_thakur>
2008-11-19 11:11:46 +0000

    Separated DAO from common Package
    SVN-Revision: 106

---
+++ src/edu/wustl/dao/daofactory/IDAOFactory.java
@@ -0,0 +1,32 @@
+package edu.wustl.dao.daofactory;
+
+import edu.wustl.common.util.dbmanager.DAOException;
+import edu.wustl.dao.DAO;
+import edu.wustl.dao.JDBCDAO;
+
+
+
+public interface IDAOFactory
+{	
+	void buildSessionFactory ()throws DAOException;
+	
+	void setDefaultDAOClassName(String defaultDAOClassName);
+	String getDefaultDAOClassName();
+	
+	void setJdbcDAOClassName(String jdbcDAOClassName);
+	String getJdbcDAOClassName();
+	
+	void setConnectionManagerName(String connectionManagerName);
+	String getConnectionManagerName();
+	
+	void setApplicationName(String applicationName);
+	String getApplicationName();
+	
+	void setConfigurationFile(String configurationFile);
+	String getConfigurationFile();
+	
+	DAO getDAO();
+	
+	JDBCDAO getJDBCDAO();
+
+}
