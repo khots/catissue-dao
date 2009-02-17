5604d77d48d3406d56953573f7c45db8d16d3862
Kalpana Thakur <kalpana_thakur>
2009-02-17 12:46:38 +0000

    Removed commented code
    SVN-Revision: 320

--- src/edu/wustl/dao/MySQLDAOImpl.java
+++ src/edu/wustl/dao/MySQLDAOImpl.java
@@ -32,48 +32,4 @@ public class MySQLDAOImpl extends AbstractJDBCDAOImpl
 
 	}
 
-
-	/**
-	 * TODO Later
-	 *This method will be called to format the SQL.
-	 *@param tableName :
-	 *@throws DAOException :Generic DAOException.
-	 *@return SQLFormatter :
-	 *//*
-	public SQLFormatter getSQLFormatter(String tableName) throws DAOException
-	{
-		return new SQLFormatterMySQL(tableName);
-	}
-
-
-	*//**
-	 * TODO Later.
-	 * @param query :
-	 * @param clobContent :
-	 *//*
-	public void updateClob(String query, String clobContent)
-	{}
-
-
-	*//**
-	 * TODO : Later.
-	 *@param sqlFormatter :
-	 *@param sequenceName :
-	 *@param columnName :
-	 *@param columnTpe :
-	 * @throws DAOException :
-	 *//*
-	public void insert(SQLFormatter sqlFormatter,String sequenceName,String columnName,
-			int columnTpe) throws DAOException
-	{
-		executeUpdate(sqlFormatter.getInsertQuery());
-		StringBuffer sqlBuff = new StringBuffer(DAOConstants.TAILING_SPACES);
-		sqlBuff.append("select max(").append(columnName).append(") from").
-		append(DAOConstants.TAILING_SPACES).append(sqlFormatter.getTableName());
-
-		Object sequenceNo = getSequenceNumber(sqlBuff.toString());
-		sqlFormatter.addColValBean(new ColumnValueBean(columnName,sequenceNo,columnTpe));
-	}
-
-*/
 }
