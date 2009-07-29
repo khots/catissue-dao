58d81bca5528c334090268abddc4b1b319534745
Kalpana Thakur <kalpana_thakur>
2009-07-29 07:47:42 +0000

    DAO_v1.0.1_29_JUL_09_AUDIT_CHANGES
    SVN-Revision: 426

---
+++ tags/DAO_v1.0.1_29_JUL_09_AUDIT_CHANGES/daojunits/dbcreation_mysql.sql
@@ -0,0 +1,60 @@
+DROP DATABASE daotestdb;
+CREATE DATABASE daotestdb;
+
+
+drop table test_order;
+drop table test_person;
+drop table test_address;
+
+
+CREATE TABLE `test_person` (                                  
+            `IDENTIFIER` bigint(20) NOT NULL auto_increment,         
+            `NAME` varchar(50) default NULL,                   
+            `ADDRESS_ID` bigint,
+            PRIMARY KEY  (`IDENTIFIER`)                              
+          ) ENGINE=InnoDB AUTO_INCREMENT=114 DEFAULT CHARSET=latin1;
+
+CREATE TABLE `test_address` (                              
+                `IDENTIFIER` bigint(20) NOT NULL auto_increment,         
+                `STREET` varchar(255) not null unique,              
+                PRIMARY KEY  (`IDENTIFIER`)                              
+            ) ENGINE=InnoDB AUTO_INCREMENT=115 DEFAULT CHARSET=latin1  ;
+
+
+alter table test_person add index person_address_index (ADDRESS_ID), 
+	add constraint person_address_index foreign key (ADDRESS_ID) references test_address (IDENTIFIER);
+
+
+
+create table test_order (
+	`IDENTIFIER` bigint(20) NOT NULL auto_increment,  
+	 PERSON_ID BIGINT,
+	 primary key (IDENTIFIER)
+)ENGINE=InnoDB AUTO_INCREMENT=114 DEFAULT CHARSET=latin1;
+
+
+alter table test_order add index person_order_index 
+	(PERSON_ID), add constraint person_order_index foreign key (PERSON_ID) references test_person (IDENTIFIER);
+
+
+drop table test_user;
+drop table test_table_hashed;
+
+CREATE TABLE `test_user` (                                                                         
+            `IDENTIFIER` bigint(20) NOT NULL auto_increment,                                                
+            `EMAIL_ADDRESS` varchar(100) default NULL,                                                      
+            `FIRST_NAME` varchar(50) default NULL,                                                          
+            `LAST_NAME` varchar(50) default NULL,                                                           
+            `ACTIVITY_STATUS` varchar(100) default NULL,                                                    
+            PRIMARY KEY  (`IDENTIFIER`)                                                                 
+          ) ENGINE=InnoDB DEFAULT CHARSET=latin1 ;                                                       
+
+CREATE TABLE test_table_hashed (                    
+                         IDENTIFIER bigint(20) NOT NULL ,        
+                         AVAILABLE tinyint,                      
+                         CREATED_ON_DATE DATE DEFAULT NULL,                    
+                         COLLECTION_STATUS VARCHAR(50) DEFAULT NULL,           
+                         PRIMARY KEY  (IDENTIFIER)                             
+                       )  ;
+insert into test_user (IDENTIFIER,EMAIL_ADDRESS,FIRST_NAME,LAST_NAME,ACTIVITY_STATUS)VALUES (1,"john@per.co.in","JOHN","REBER","Active");
+insert into test_user (IDENTIFIER,EMAIL_ADDRESS,FIRST_NAME,LAST_NAME,ACTIVITY_STATUS)VALUES (2,"sri@per.co.in","srikanth",null,"Active");
---
+++ tags/DAO_v1.0.1_29_JUL_09_AUDIT_CHANGES/src/edu/wustl/dao/OracleDAOImpl.java
@@ -0,0 +1,56 @@
+/*
+ * This will hold the implementation specific to Oracle.
+ */
+
+package edu.wustl.dao;
+
+import edu.wustl.common.util.logger.Logger;
+import edu.wustl.dao.exception.DAOException;
+import edu.wustl.dao.util.DAOUtility;
+
+
+/**
+ * @author kalpana_thakur
+ *
+ */
+public class OracleDAOImpl extends AbstractJDBCDAOImpl
+{
+
+	/**
+	 * Logger.
+	 */
+	private static org.apache.log4j.Logger logger = Logger.getLogger(OracleDAOImpl.class);
+
+	/**
+	 * Deletes the specified table.
+	 * @param tableName : Name of table to be deleted.
+	 * @throws DAOException : DAOException.
+	 */
+	public void deleteTable(String tableName) throws DAOException
+	{
+		try
+		{
+
+			StringBuffer query = new StringBuffer("select tname from tab where tname='"
+					+ tableName.toString() + "'");
+			boolean isTableExists = DAOUtility.getInstance().
+			isResultSetExists(getQueryResultSet(query.toString()));
+
+			logger.debug("ORACLE :" + query.toString() + isTableExists);
+
+			if (isTableExists)
+			{
+
+				logger.debug("Drop Table");
+				executeUpdate("DROP TABLE " +
+						tableName.toString() + " cascade constraints");
+			}
+
+		}
+		finally
+		{
+			closeConnectionParams();
+		}
+	}
+
+}
