4dc1ec77fa722bc2e5ac825c930c82f2f1ebb2b2
nightlybuild <nightlybuild>
2010-01-19 16:43:06 +0000

    functionality checked: audit xml generation junit: yes
    SVN-Revision: 506

--- daojunits/src/edu/wustl/dao/test/DAOTestSuite.java
+++ daojunits/src/edu/wustl/dao/test/DAOTestSuite.java
@@ -36,6 +36,8 @@ public class DAOTestSuite
 		TestSuite daoSuite = new TestSuite("Test for DAO Layer");
 		daoSuite.addTestSuite(HibernateTestCaseForCatissue.class);
 		daoSuite.addTestSuite(JDBCTestCasesForCatissue.class);
+		daoSuite.addTestSuite(TestAuditXMLGenerator.class);
+
 		return daoSuite;
 	}
 
