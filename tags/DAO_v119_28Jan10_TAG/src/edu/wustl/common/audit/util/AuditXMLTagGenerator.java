3c270e8379de3cd4fff1d36c49c67397dae1a399
Kalpana Thakur <kalpana_thakur>
2010-01-28 10:22:25 +0000

    Audit issue and removed ' and -- from invalid character list.
    SVN-Revision: 539

---
+++ tags/DAO_v119_28Jan10_TAG/src/edu/wustl/common/audit/util/AuditXMLTagGenerator.java
@@ -0,0 +1,153 @@
+
+package edu.wustl.common.audit.util;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * This class generates the tag used metadata audit xml
+ * @author kunal_kamble
+ *
+ */
+public class AuditXMLTagGenerator
+{
+	/**
+	 * @param className
+	 * @return
+	 */
+	public String getAuditableMetatdataXMLString(String className)
+	{
+		StringBuffer auditableMetatdataXML = new StringBuffer();
+		auditableMetatdataXML.append(getAuditableClassTag(className));
+		auditableMetatdataXML.append('\n');
+		try
+		{
+			Class newObjectClass = Class.forName(className);
+			Map<String, Field> fieldList = new HashMap<String, Field>();
+			populateAllFields(newObjectClass, fieldList);
+			for (Field field : fieldList.values())
+			{
+				if (!"serialVersionUID".equalsIgnoreCase(field.getName()) && validateAttribute(field,newObjectClass))
+				{
+					auditableMetatdataXML.append('\t');
+					updateAttributeTag(auditableMetatdataXML,field);
+				}
+
+			}
+		}
+		catch (ClassNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			auditableMetatdataXML.append("</AuditableClass>");
+		}
+		return auditableMetatdataXML.toString();
+	}
+
+	/**
+	 * check whether getter present for the given attribute in the class or not
+	 * @param field
+	 * @param newObjectClass
+	 * @return
+	 */
+	private boolean validateAttribute(Field field, Class newObjectClass)
+	{
+		boolean validAttribute = false;
+
+		String fieldName = field.getName();
+		Method methlist[] = newObjectClass.getDeclaredMethods();
+		for(Method method: methlist)
+		{
+			if(method.getName().equalsIgnoreCase("get"+fieldName))
+			{
+				validAttribute = true;
+			}
+		}
+		return validAttribute;
+	}
+
+	/**
+	 * @param klass
+	 * @param fieldList field list declared in the class and in its parent classes
+	 */
+	private void populateAllFields(Class klass, Map<String, Field> fieldList)
+	{
+		if (klass.getSuperclass() != null)
+		{
+			populateAllFields(klass.getSuperclass(), fieldList);
+		}
+		klass.getDeclaredFields();
+		for (Field field : klass.getDeclaredFields())
+		{
+			fieldList.put(field.getName(), field);
+		}
+	}
+
+	/**
+	 * @param className
+	 * @return Auditable class tag
+	 */
+	private String getAuditableClassTag(String className)
+	{
+		String relationshipType = "main";
+		String roleName = "";
+		String auditableClass = AuditXMLConstants.AUDIT_CLASS_TAG;
+		auditableClass = auditableClass.replace(AuditXMLConstants.CLASS_NAME_TOKEN, className);
+		auditableClass = auditableClass.replace(AuditXMLConstants.ROLE_NAME_TOKEN, roleName);
+		auditableClass = auditableClass.replace(AuditXMLConstants.RELATIONSHIP_TYPE_TOKEN,
+				relationshipType);
+
+		return auditableClass;
+
+	}
+
+	/**
+	 * @param field
+	 * @return attribute tag
+	 */
+	private void updateAttributeTag(StringBuffer auditableMetatdataXML,Field field)
+	{
+
+		if (!AuditXMLGenerator.excludeAssociation && field.getType().getName().startsWith("java.util.")
+				&& !field.getType().getName().startsWith("java.util.Date"))
+		{
+			auditableMetatdataXML.append(getContainmentAssociationCollection(field));
+			auditableMetatdataXML.append('\n');
+		}
+		else if(!field.getType().getName().startsWith("java.util.Collection"))
+		{
+
+			String fieldName = field.getName();
+			String fieldType = field.getType().getName();
+			String attributeTag = AuditXMLConstants.ATTRIBUTE_TAG;
+			attributeTag = attributeTag.replace(AuditXMLConstants.NAME_TOKEN, fieldName);
+			attributeTag = attributeTag.replace(AuditXMLConstants.DATA_TYPE_TOKEN, fieldType);
+			auditableMetatdataXML.append(attributeTag);
+			auditableMetatdataXML.append('\n');
+		}
+	}
+
+	/**
+	 * @param field
+	 * @return Containment association collection tag
+	 */
+	private String getContainmentAssociationCollection(Field field)
+	{
+		String containmentAssociationTag = AuditXMLConstants.CONTAINMENT_ASSOCIATION_COLLECTION_TAG;
+		String relationshipType = "containment";
+		String roleName = field.getName();
+		String className = field.getType().getName();
+
+		containmentAssociationTag = containmentAssociationTag.replace(
+				AuditXMLConstants.CLASS_NAME_TOKEN, className);
+		containmentAssociationTag = containmentAssociationTag.replace(
+				AuditXMLConstants.RELATIONSHIP_TYPE_TOKEN, relationshipType);
+		containmentAssociationTag = containmentAssociationTag.replace(
+				AuditXMLConstants.ROLE_NAME_TOKEN, roleName);
+		return containmentAssociationTag;
+	}
+}
---
+++ tags/DAO_v119_28Jan10_TAG/src/edu/wustl/dao/HibernateDAO.java
@@ -0,0 +1,58 @@
+/**
+ * <p>Title: HibernateDAO Interface</p>
+ * <p>Description :This interface defines methods which are specific to
+ * Hibernate operations</p>
+ *  * @author kalpana_thakur
+ */
+package edu.wustl.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import edu.wustl.dao.exception.DAOException;
+import edu.wustl.dao.util.HibernateMetaData;
+import edu.wustl.dao.util.NamedQueryParam;
+
+
+/** This interface defines methods which are specific to Hibernate operations .*/
+
+public interface HibernateDAO extends DAO
+{
+
+	/**
+	 * Obtain an instance of Query for a named query string defined in the mapping file.
+	 * @param queryName : handle for named query.
+	 * @param namedQueryParams : Map holding the parameter type and parameter value.
+	 * @return the list of data.
+	 * @throws DAOException :Generic DAOException.
+	 */
+	List executeNamedQuery(String queryName,Map<String, NamedQueryParam> namedQueryParams) throws DAOException;
+
+	/**
+	 * Executes the HQL query. for given startIndex and max
+	 * records to retrieve
+	 * @param query  HQL query to execute
+	 * @param startIndex Starting index value
+	 * @param maxRecords max number of records to fetch
+	 * @param paramValues List of parameter values.
+	 * @return List of data.
+	 * @throws DAOException database exception.
+	 */
+	List executeQuery(String query,Integer startIndex,
+			Integer maxRecords,List paramValues) throws DAOException;
+
+	/**
+	 * This method will be called to set the hibernate metadata
+	 * for the application.
+	 * @param hibernateMetaData the hibernateMetaData to set
+	 */
+	void setHibernateMetaData(HibernateMetaData hibernateMetaData);
+
+	/**
+	 * This method will be called to fetch hibernate metadata
+	 * associated to the application.
+	 * @return the hibernateMetaData
+	 */
+	HibernateMetaData getHibernateMetaData();
+
+}
