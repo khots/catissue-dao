37b091f6d02e0c7bab9e89e20b7cc9ab1ccd4b53
Srikanth Adiga <srikanth.adiga@gmail.com>
2011-05-25 05:13:00 +0000

    branch for caTissue release 1.2.1.wu
    SVN-Revision: 611

---
+++ branches/DAO_1.2.1.wu/daojunits/src/test/Person.java
@@ -0,0 +1,104 @@
+package test;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+/**
+ * @author kalpana_thakur
+ *
+ */
+public class Person
+{
+	/**
+	 * id.
+	 */
+    private Long id;
+
+    /**
+     * name.
+     */
+    private String name;
+
+
+    /**
+     * The address of the site.
+     */
+	private Address address;
+
+
+	/**
+	 * person's order collection.
+	 */
+	private Collection<Object> orderCollection = new HashSet<Object>();
+
+    /**
+     * @return collection of orders.
+     */
+    public Collection<Object> getOrderCollection()
+    {
+		return orderCollection;
+	}
+
+	/**
+	 * @param orderCollection :
+	 */
+	public void setOrderCollection(Collection<Object> orderCollection)
+	{
+		this.orderCollection = orderCollection;
+	}
+
+	/**
+     *@return person name.
+     */
+    public String getName()
+	{
+		return name;
+	}
+
+    /**
+     * @param name : name of person
+     */
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	/**
+	 * @return identifier.
+	 */
+    public Long getId()
+    {
+		return id;
+	}
+
+    /**
+	 * @param identifier :
+	 */
+	public void setId(Long identifier)
+	{
+		this.id = identifier;
+	}
+
+
+
+	/**
+	 * Returns the address.
+	 * @return Address
+	*/
+	public Address getAddress()
+	{
+		return address;
+	}
+
+	/**
+	 * Sets the address.
+	 * @param address address
+	 */
+	public void setAddress(Address address)
+	{
+		this.address = address;
+	}
+
+
+}
+
