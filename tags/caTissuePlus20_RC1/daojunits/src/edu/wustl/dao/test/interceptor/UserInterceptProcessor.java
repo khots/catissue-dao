ad6ac428e75326e4ee5562dd1e5002b78a6063b0
Pathik Sheth <psheth>
2012-12-06 07:09:00 +0000

    created new tag for catissue plus RC1 release.
    SVN-Revision: 677

---
+++ tags/caTissuePlus20_RC1/daojunits/src/edu/wustl/dao/test/interceptor/UserInterceptProcessor.java
@@ -0,0 +1,31 @@
+package edu.wustl.dao.test.interceptor;
+
+import edu.wustl.dao.interceptor.InterceptProcessor;
+import edu.wustl.dao.interceptor.SaveUpdateInterceptThread.eventType;
+
+public class UserInterceptProcessor implements InterceptProcessor{
+
+	public static final String IS_PROCESSOR_EXECUTED_INSERT="INTERCEPT_PROCESSOR_INSERT";
+	public static final String IS_PROCESSOR_EXECUTED_UPDATE="INTERCEPT_PROCESSOR_INSERT";
+	public void process(Object arg0, eventType type)
+	{
+		System.out.println(" in person intercept processor");
+		// TODO Auto-generated method stub
+		if(eventType.onInsert.equals(type))
+		{
+			//System.setProperty(IS_PROCESSOR_EXECUTED_INSERT, "true");
+		}
+		else
+		{
+			//System.setProperty(IS_PROCESSOR_EXECUTED_UPDATE, "true");
+		}
+
+	}
+	public void onError(String objectType, eventType type, Long objectId)
+	{
+		// TODO Auto-generated method stub
+		System.out.println("in error recovery method");
+
+	}
+
+}
